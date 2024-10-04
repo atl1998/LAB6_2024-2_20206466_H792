15f15b9002610d0b31ad16b1d4f54b6b70fdabfc
atl1998 <167376701+atl1998@users.noreply.github.com>
2024-10-03 21:45:49 -0500

    uu

--- lab6_20206466/src/main/java/com/example/lab6_20206466/controller/AdminController.java
+++ lab6_20206466/src/main/java/com/example/lab6_20206466/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
 import java.util.Optional;
 @Controller
-@RequestMapping("/index")
+@RequestMapping("index")
 public class AdminController {
     @Autowired
     DirectorRepository directorRepository;
